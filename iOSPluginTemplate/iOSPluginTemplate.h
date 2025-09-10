54569a1439df9e33897541be074622a065f33e49
George Marques <george@gmarqu.es>
2025-09-10 10:04:36 -0300

    Initial Commit

---
+++ iOSPluginTemplate/iOSPluginTemplate.h
@@ -0,0 +1,12 @@
+//
+//  iOSPluginTemplate.h
+//  iOSPluginTemplate
+//
+//  Created by George Marques on 10/09/25.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface iOSPluginTemplate : NSObject
+
+@end
---
+++ iOSPluginTemplate/iOSPluginTemplate.m
@@ -0,0 +1,12 @@
+//
+//  iOSPluginTemplate.m
+//  iOSPluginTemplate
+//
+//  Created by George Marques on 10/09/25.
+//
+
+#import "iOSPluginTemplate.h"
+
+@implementation iOSPluginTemplate
+
+@end
