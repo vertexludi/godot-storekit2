40cf7f7d42fbec594560827dab8eb453663cb3aa
George Marques <george@gmarqu.es>
2025-09-11 11:03:19 -0300

    Add stub API

--- godot-storekit2/godot-storekit2.h
+++ godot-storekit2/godot-storekit2.h
@@ -2,11 +2,22 @@
 
 #include "core/object/ref_counted.h"
 
+#include "core/string/ustring.h"
+#include "core/variant/dictionary.h"
+
 class GodotStoreKit2 : public RefCounted {
 	GDCLASS(GodotStoreKit2, RefCounted)
+
+	bool initialized = false;
 
 	static void _bind_methods();
 
 public:
-	void hello_world();
+	void initialize();
+	bool is_initialized() const;
+	bool is_product_available(String p_product_id);
+	bool is_product_purchased(String p_product_id);
+	Dictionary get_product_price(String p_product_id);
+	void purchase_product(String p_product_id, int p_quantity = 1);
+	void restore_purchases();
 };
--- godot-storekit2/godot-storekit2.mm
+++ godot-storekit2/godot-storekit2.mm
@@ -5,9 +5,40 @@
 @import Foundation;
 
 void GodotStoreKit2::_bind_methods() {
-	ClassDB::bind_method(D_METHOD("hello_world"), &GodotStoreKit2::hello_world);
+	ClassDB::bind_method(D_METHOD("initialize"), &GodotStoreKit2::initialize);
+	ClassDB::bind_method(D_METHOD("is_initialized"), &GodotStoreKit2::is_initialized);
+	ClassDB::bind_method(D_METHOD("is_product_available", "product_id"), &GodotStoreKit2::is_product_available);
+	ClassDB::bind_method(D_METHOD("is_product_purchased", "product_id"), &GodotStoreKit2::is_product_purchased);
+	ClassDB::bind_method(D_METHOD("get_product_price", "product_id"), &GodotStoreKit2::get_product_price);
+	ClassDB::bind_method(D_METHOD("purchase_product", "product_id", "quantity"), &GodotStoreKit2::purchase_product, DEFVAL(1));
+
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "initialized"), "", "is_initialized");
+
 }
 
-void GodotStoreKit2::hello_world() {
-	print_line("Hello world!");
+void GodotStoreKit2::initialize() {
+	initialized = true;
 }
+
+bool GodotStoreKit2::is_initialized() const {
+	return initialized;
+}
+
+bool GodotStoreKit2::is_product_available(String p_product_id) {
+	return false;
+}
+
+bool GodotStoreKit2::is_product_purchased(String p_product_id) {
+	return false;
+}
+
+Dictionary GodotStoreKit2::get_product_price(String p_product_id) {
+	Dictionary result;
+
+	return result;
+}
+
+void GodotStoreKit2::purchase_product(String p_product_id, int p_quantity) {}
+
+void GodotStoreKit2::restore_purchases() {}
+
